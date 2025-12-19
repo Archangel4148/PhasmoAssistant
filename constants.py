4c92f4d8c52bbb2eb05b57b3e6c4843bac83e490
Simon Edmunds <sjebkc@umsystem.edu>
2025-12-19 12:44:49 -0600

    Setting up basic VOSK testing with keywords

---
+++ constants.py
@@ -0,0 +1,14 @@
+
+
+
+RECOGNIZED_KEYWORDS = [
+  "confirm ghost orbs",
+  "confirm dots",
+  "confirm oni",
+  "confirm yokai",
+  "trigger oni",
+  "trigger yokai",
+  "trigger enable overlay",
+  "trigger dark mode",
+  "trigger narration level three"
+]
